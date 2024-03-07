066d4f13b856f62b8247d4d7f98fa7da7a02033f
SURBHI-SARAF <91864439+SURBHI-SARAF@users.noreply.github.com>
2024-03-07 21:41:26 +0530

    Add files via upload

---
+++ MyJavaScript.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+document.write("hii Surbhi");
+
+
+alert("Good Morning !");
+document.getElementById("heading1").innerHTML="Hey Everyone!"
+document.getElementById("para2").innerText=3*5;
+var Ronit;
+Ronit = 30;
+var Ronit1;
+Ronit1 = 2;
+var mult = Ronit*Ronit1;
+document.getElementById("para4").innerHTML = mult;
+var name = "Surbhi Saraf";
+var age = 22;
+
+document.write(name+ "is" +age+ "years old");
+//write a document to get the area of circle of any radius and print the result in para
+var pi = 3.14;
+var radius = 5;
+result = pi*radius*radius;
+document.getElementById("para5").innerHTML = result;
---
+++ first.js
@@ -0,0 +1,24 @@
+name="Tony stark";
+console.log(name);
+fullname  = "surtik shah";
+FULLNAME = "SURTIK SHAH";
+console.log(fullname);
+
+const product =  {
+    productname : "cello pen",
+    rating : 5,
+    offer : 7,
+    price : 260
+};
+console.log(product);
+
+let a = 6, b = 8;
+console.log("a++",a++);//post increment
+console.log(a);
+console.log("a--",a--);//post decrement
+console.log(a);
+
+ console.log("++a",++a);//pre increment
+ console.log(a);
+console.log("--a",--a);//pre decrement
+ console.log(a);
